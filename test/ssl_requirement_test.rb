aa7f70420a282acc77050ea5d7d8abc8ac8bca71
bmpercy <percival@umamibud.com>
2009-06-23 20:36:27 -0700

    finally got the flash tests working under Rails 2.3.2

--- test/ssl_requirement_test.rb
+++ test/ssl_requirement_test.rb
@@ -26,6 +26,12 @@ require "#{File.dirname(__FILE__)}/../lib/ssl_requirement"
 ActionController::Base.logger = nil
 ActionController::Routing::Routes.reload rescue nil
 
+# several test controllers to cover different combinations of requiring/
+# allowing/exceptions-ing SSL for controller actions
+
+# this first controller modifies the flash in every action so that flash
+# set in set_flash is eventually expired (see NOTE below...)
+
 class SslRequirementController < ActionController::Base
   include SslRequirement
   
@@ -33,21 +39,25 @@ class SslRequirementController < ActionController::Base
   ssl_allowed :c
   
   def a
+    flash[:abar] = "foo"
     render :nothing => true
   end
   
   def b
+    flash[:bbar] = "foo"
     render :nothing => true
   end
   
   def c
+    flash[:cbar] = "foo"
     render :nothing => true
   end
   
   def d
+    flash[:dbar] = "foo"
     render :nothing => true
   end
-  
+
   def set_flash
     flash[:foo] = "bar"
   end
@@ -76,9 +86,6 @@ class SslExceptionController < ActionController::Base
     render :nothing => true
   end
   
-  def set_flash
-    flash[:foo] = "bar"
-  end
 end
 
 class SslAllActionsController < ActionController::Base
@@ -92,41 +99,69 @@ class SslAllActionsController < ActionController::Base
   
 end
 
-class SslRequirementTest < Test::Unit::TestCase
+# NOTE: The only way I could get the flash tests to work under Rails 2.3.2
+#       (without resorting to IntegrationTest with some artificial session
+#       store) was to use TestCase. In TestCases, it appears that flash 
+#       messages are effectively persisted in session after the last controller
+#       action that consumed them...so that when the TestCase inspects
+#       the FlashHash, it will find the flash still populated, even though
+#       the subsequent controller action won't see it.
+#
+#       In addition, if no changes are made to flash in subsequent requests, the
+#       flash is persisted forever. But if subsequent controller actions add to
+#       flash, the older flash messages eventually disappear.
+#
+#       As a result, the flash-related tests now make two requests after the 
+#       set_flash, each of these requests is also modifying flash. flash is 
+#       inspected after the second request returns.
+#
+#       This feels a little hacky, so if anyone can improve it, please do so!
+
+class SslRequirementTest < ActionController::TestCase
   def setup
     @controller = SslRequirementController.new
-    @request    = ActionController::TestRequest.new
-    @response   = ActionController::TestResponse.new
+    @ssl_host_override = 'www.example.com:80443'
+    @non_ssl_host_override = 'www.example.com:8080'
   end
   
   def test_redirect_to_https_preserves_flash
+    assert_not_equal "on", @request.env["HTTPS"]
     get :set_flash
     get :b
-    assert_response :redirect
-    assert_equal "bar", flash[:foo]
+    assert_response :redirect       # check redirect happens (flash still set)
+    get :b                          # get again to flush flash
+    assert_response :redirect       # make sure it happens again
+    assert_equal "bar", flash[:foo] # the flash would be gone now if no redirect
   end
   
   def test_not_redirecting_to_https_does_not_preserve_the_flash
+    assert_not_equal "on", @request.env["HTTPS"]
     get :set_flash
     get :d
-    assert_response :success
-    assert_nil flash[:foo]
+    assert_response :success        # check no redirect (flash still set)
+    get :d                          # get again to flush flash
+    assert_response :success        # check no redirect
+    assert_nil flash[:foo]          # the flash should be gone now
   end
   
   def test_redirect_to_http_preserves_flash
     get :set_flash
     @request.env['HTTPS'] = "on"
     get :d
-    assert_response :redirect
-    assert_equal "bar", flash[:foo]
+    assert_response :redirect       # check redirect happens (flash still set)
+    get :d                          # get again to flush flash
+    assert_response :redirect       # make sure redirect happens
+    assert_equal "bar", flash[:foo] # flash would be gone now if no redirect
   end
   
   def test_not_redirecting_to_http_does_not_preserve_the_flash
     get :set_flash
     @request.env['HTTPS'] = "on"
     get :a
-    assert_response :success
-    assert_nil flash[:foo]
+    assert_response :success        # no redirect (flash still set)
+    get :a                          # get again to flush flash
+    assert_response :success        # no redirect
+    assert_nil flash[:foo]          # flash should be gone now
   end
   
   def test_required_without_ssl
@@ -155,13 +190,13 @@ class SslRequirementTest < Test::Unit::TestCase
   
   def test_ssl_exceptions_without_ssl
     @controller = SslExceptionController.new
+
+    assert_not_equal "on", @request.env["HTTPS"]
     get :a
     assert_response :redirect
     assert_match %r{^https://}, @response.headers['Location']
-    
     get :b
     assert_response :success
-    
     get :c # c is not explicity in ssl_required, but it is not listed in ssl_exceptions
     assert_response :redirect
     assert_match %r{^https://}, @response.headers['Location']
@@ -172,8 +207,6 @@ class SslRequirementTest < Test::Unit::TestCase
     @request.env['HTTPS'] = "on"
     get :a
     assert_response :success
-    
-    @request.env['HTTPS'] = "on"
     get :c
     assert_response :success
   end
@@ -181,7 +214,6 @@ class SslRequirementTest < Test::Unit::TestCase
   def test_ssl_all_actions_without_ssl
     @controller = SslAllActionsController.new
     get :a
-    
     assert_response :redirect
     assert_match %r{^https://}, @response.headers['Location']
   end
@@ -216,5 +248,5 @@ class SslRequirementTest < Test::Unit::TestCase
   ensure
     SslRequirement.disable_ssl_check = false
   end
-  
+
 end
